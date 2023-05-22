c78f3a63f5dcc3c4074baf45a912a99622d730ff
Yuh-Boh Feng <fengyuhboh@gmail.com>
2023-05-21 19:29:45 -0700

    started working on boilerplate to connect with my database

--- database.sql
+++ database.sql
@@ -8,3 +8,40 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE user_profile (
+	id SERIAL PRIMARY KEY,
+	profile_pic Varchar (255) Not Null,
+	channel_name Varchar (255) Not Null,
+	about Varchar (255) Not Null,
+	user_id Integer
+);
+
+CREATE TABLE user_profile_video (
+	id SERIAL PRIMARY KEY,
+	user_profile_id Integer,
+	user_id Integer
+);
+
+CREATE TABLE video (
+	id SERIAL PRIMARY KEY,
+	video_url Varchar (1000),
+	thumbnail Varchar (255),
+	video_time Varchar (255),
+	channel_pic Varchar (255),
+	video_author Varchar (255),
+	video_subscribers Integer,
+	video_stats Varchar (255) 
+);
+
+CREATE TABLE playlist (
+	id SERIAL PRIMARY KEY,
+	playlist_name Varchar (255),
+	user_profile_id Integer
+);
+
+CREATE TABLE playlist_video (
+	id SERIAL PRIMARY KEY,
+	playlist_id integer,
+	video_id integer
+);
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
+require("dotenv").config();
 const bodyParser = require('body-parser');
-require('dotenv').config();
+
 
 const app = express();
 

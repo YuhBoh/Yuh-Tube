95a86424e3aa29da294afc7d02cca5e8f0d1f277
Yuh-Boh Feng <fengyuhboh@gmail.com>
2023-05-29 23:11:51 -0700

    last css changes for tonight

--- src/components/UserPage/VideoItem.jsx
+++ src/components/UserPage/VideoItem.jsx
@@ -5,15 +5,35 @@ import VideoOptions from './VideoOptions/VideoOptions';
 export default function VideoItem({video}) {
 
   return (
-      <div className='thumbnail-row'>
-        <a href={video.video_url} target="_blank">
-          <img className="thumbnail"
-               src={require("./images/thumbnail.png")} alt="Youtube-Link" />
-        </a> 
+    <>
+      <div className='video-preview'>
+        <div className="thumbnail-row">
+          
+          <a href={video.video_url} target="_blank">
+            <img className="thumbnail"
+                src={require("./images/thumbnail.png")} alt="Youtube-Link" />
+          </a> 
 
-        <div className='video-options'>
-          <VideoOptions video={video}/>
+          <div className='video-options'>
+            <VideoOptions video={video}/>
+          </div>
         </div>
+
+        <div className="video-info-grid">
+
+          <div className="video-info">
+            <a href={video.video_url}>
+              <p className="video-title">
+                {video.video_url}
+              </p>
+            </a>
+          </div>
+          
+        </div>
+
       </div>
+    </>
+      
+        
   )
 }

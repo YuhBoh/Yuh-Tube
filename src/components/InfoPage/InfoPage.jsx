fe1245f3a7e5ba353898282f5f1b88d01d9f4ed3
Yuh-Boh Feng <fengyuhboh@gmail.com>
2023-06-04 13:11:42 -0700

    css change to the background

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./InfoPage.css";
 
 // This is one of our simplest components
 // It doesn't have local state
@@ -8,7 +9,27 @@ import React from 'react';
 function InfoPage() {
   return (
     <div className="about-container">
-      <p>Info Page</p>
+      <h3>Info Page</h3>
+
+      <p className="section-title">Technologies</p>
+      <ul>
+        <li>HTML/CSS</li>        
+        <li>Javascript</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>React</li>
+        <li>Redux</li>
+        <li>Saga</li>
+        <li>SQL</li>
+        <li>Passport</li>
+        <li>Postico</li>
+        <li>Postgresql</li>
+        <li>Postman</li>
+        <li>Git</li>
+        <li>Github</li>
+        <li>Google Cloud</li>
+          <li>Youtube Data API v3</li>
+      </ul>
     </div>
   );
 }

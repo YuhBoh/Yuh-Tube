c7cd9ba92b12618b2d584a7949ef29cff9213522
Yuh-Boh Feng <fengyuhboh@gmail.com>
2023-09-11 13:13:48 -0700

    added pg url

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -26,10 +26,11 @@ if (process.env.DATABASE_URL) {
 // also running on our computer (localhost)
 else {
     pool = new pg.Pool({
-        host: 'localhost',
-        port: 5432,
-        database: 'solo-project',   // 	💥 Change this to the name of your database!
-        // postgres:'//solo_project_user:71fhKUaSC1elKt8SfPQt1QKMH4l0lwta@dpg-cjtotofjbvhs73asdvag-a.oregon-postgres.render.com/solo_project'
+      host: "localhost",
+      port: 5432,
+      database: "solo-project", // 	💥 Change this to the name of your database!
+      // postgres:'//solo_project_user:71fhKUaSC1elKt8SfPQt1QKMH4l0lwta@dpg-cjtotofjbvhs73asdvag-a.oregon-postgres.render.com/solo_project'
+      DATABASE_URL: 'postgres://qygczlvzfiegoz:9c965eb98907a2d236d8254e6c30e3ec6e0adf48a1687128f9c548bad3b903a5@ec2-52-45-200-167.compute-1.amazonaws.com:5432/dcjam7f25i4a2s',
     });
 }
 

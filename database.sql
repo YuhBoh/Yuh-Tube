d5d8ce9204565de26639ad4d0774cb5b0bd900b8
Yuh-Boh Feng <fengyuhboh@gmail.com>
2023-09-11 16:03:27 -0700

    changed sql file

--- database.sql
+++ database.sql
@@ -51,3 +51,58 @@ CREATE TABLE playlist_video (
 	video_id int, 
 	foreign key (video_id) references video(id)
 );
+
+alter table user_profile_video
+rename to user_video;
+
+alter table user_video
+add foreign key (user_id) references "user"(id);
+
+alter table playlist
+add foreign key (user_id) references "user"(id);
+
+alter table "user"
+add column profile_pic varchar (255),
+add column channel_name varchar (255),
+add column about varchar (255)
+
+alter table playlist
+rename column user_profile_id to user_id;
+
+alter table user_profile_video
+rename column user_profile_id to user_id;
+
+insert into user_profile
+(profile_pic, channel_name, about, user_id)
+values
+(1, 1, 1, 1)
+
+insert into user_profile_video
+(user_profile_id, video_id)
+values
+(1, 1)
+
+SELECT video_url 
+FROM "video"
+WHERE "id"=1;
+
+SELECT id, video_url 
+FROM "video"
+
+select *
+from playlist;
+
+alter table video
+ADD video_title varchar(255);
+
+alter table video
+rename column video_stats to published_at;
+
+alter table video
+rename column video_subscribers to subscription_count;
+
+alter table video
+rename column video_author to channel_title;
+
+alter table video
+rename column video_time to time;
